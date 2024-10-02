7af37828cbcae241cbe53a81f598de9a20f7fe61
Tautik05 <tautiksinharoy@gmail.com>
2024-10-02 13:43:56 +0530

    updated requirements.text and created a Procfile

--- food/settings.py
+++ food/settings.py
@@ -23,9 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-787!lb)3qfpc=9n@(0jxkj-ft#ppz4ycc!ip7dkfa6smzsc*p0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+
+DEBUG = False
+
+# ALLOWED_HOSTS = []
+
+
+
+ALLOWED_HOSTS = ['food.onrender.com']
 
 # Application definition
 
